932c37aa078afd2f016e5f99f4f2a8152627e46b
5hawon <sshawon993@gmail.com>
2024-05-22 22:44:13 +0600

    .

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool(
         user: "postgres",
         host:'localhost',
         database: 'students',
-        password: 'shawon',
+        password: '',//your password
         port: 5432,
 
     }
